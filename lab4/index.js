7e25a3ebe5143f1cbc1937b661bcc6128c274a8b
fleep-6 <pcrans200@caledonian.ac.uk>
2020-02-17 10:50:46 +0000

    Lab4

---
+++ lab4/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+
+var mustache = require('mustache-express'),
+    path = require('path');
+
+
+app.engine('mustache', mustache());
+
+app.set('port', process.env.PORT || 5000);
+app.set('view engine', 'mustache');
+app.set('views', path.resolve(__dirname, 'mustache'));
+
+app.get("/", function(request, response) {
+    response.status(200);
+    response.type('text/html');
+    response.send('<h1>Landing Page</h1>');
+});
+
+app.get("/page", function(request, response) {
+    response.render("page", {
+        'title': 'New Guest Book',
+        'subject': 'Good day out',
+        'review': 'We had a really good time visiting the museum.'
+    });
+});
+
+app.use(function(request, response) {
+    response.type('text/plain');
+    response.status(404);
+    response.send("Bad luck, 404");
+});
+
+app.listen(app.get('port'), function() {
+    console.log('server running');
+});
