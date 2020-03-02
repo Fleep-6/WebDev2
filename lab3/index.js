fbc3833b9f517e3b63aeaed0796f557ed8f0ca23
fleep-6 <pcrans200@caledonian.ac.uk>
2020-03-02 10:42:35 +0000

    cleanup

--- lab3/index.js
+++ lab3/index.js
@@ -1,16 +1,12 @@
-//lab 3
 var express = require('express');
-var mustache = require('mustache-express');
-var path = require('path');
-
-var app = express();
 var mustache = require('mustache-express'),
     path = require('path');
 
-
-app.engine('mustache', mustache());
+var app = express();
 
 app.set('port', process.env.PORT || 5000);
+
+app.engine('mustache', mustache());
 app.set('view engine', 'mustache');
 app.set('views', path.resolve(__dirname, 'mustache'));
 
@@ -23,14 +19,19 @@ app.get("/", function(request, response) {
 app.get("/page", function(request, response) {
     response.render("page", {
         'title': 'New Guest Book',
-        'subject': 'Good day out',
-        'review': 'We had a really good time visiting the museum.',
-        'visitors': [
-            { 'name': 'Peter' },
-            { 'name': 'Adam' },
-            { 'name': 'Bella' }
+        'entries': [{
+                'subject': 'good day out',
+                'review': 'We had a really good time visiting the museum.'
+            },
+            {
+                'subject': 'Liked the exhibition',
+                'review': 'Good to see the Glasgow Boys again.'
+            },
+            {
+                'subject': 'Magical few hours',
+                'review': 'Magnificant looking building.'
+            }
         ]
-
     });
 });
 
